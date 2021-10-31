15ca8fe5196e68310bdcd85c01d305003dad8bb4
Michael Leung <kmleung@live.com>
2021-10-31 15:28:49 +0800

    initial submit

---
+++ Ehome.ino
@@ -0,0 +1,328 @@
+// EmonLibrary examples openenergymonitor.org, Licence GNU GPL V3
+#include "Arduino.h"
+#include "EmonLib.h"
+#include <TM1637Display.h>
+#include <WiFi.h>
+#include "ESPAsyncWebServer.h"
+#include "time.h"
+#include "SD.h"
+#include "SPI.h"
+#include "Preferences.h"
+//#include "soc/soc.h"
+//#include "soc/rtc_cntl_reg.h"
+
+#define TM1637_CLK 15
+#define TM1637_DIO 16
+#define CT_ADC 36
+#define SD_CS_Pin 5
+#define SD_MOSI_Pin 23
+#define SD_MISO_Pin 19
+#define SD_SCK_Pin 18
+#define buzzer 26
+#define cycle_factory 1480
+// 1sec=17982, 5 cycles @ 50 Hz =100ms=1798 18cycle=300ms=5394 old=1480,1676
+
+const int   led = LED_BUILTIN;
+const char* ssid     = "xxxxxxxx";
+const char* password = "yyyyyyyy";
+const char* ntpServer = "hk.pool.ntp.org";
+const long  gmtOffset_sec = 28800;
+const int   daylightOffset_sec = 0;
+const int   alert_factory = 3300; //33.00A
+const int   CT_ADJ_factory = 600;
+const int   changed_A = 50; // 0.5A
+const uint8_t OFF[] = {0x00};
+
+struct tm timeinfo;
+EnergyMonitor emon1;                   // Create an instance
+Preferences preferences;
+hw_timer_t * timer = NULL;
+TM1637Display display(TM1637_CLK, TM1637_DIO);
+AsyncWebServer server(80);
+File history;
+File historyAction;
+
+volatile byte state = LOW;
+volatile int cycle_cnt = 0;
+int Irms = 0;
+int last_Irms = 0001; //avoid div by zero
+int changed_duration = 10; // for 10 seconds
+bool changed = false;
+bool buzzer_ff = false;
+int len, pos;
+uint8_t dot;
+double CT_ADJ;
+String Webmessage;
+int alert;
+int cycle;
+int reconnect_cnt = 0;
+int start;
+
+void IRAM_ATTR onTimer() {
+  state = !state;
+  cycle_cnt += 1;
+}
+
+void initwifi() {
+  bool timesync = false;
+  while (!timesync) {
+    int retrycount = 0;
+    Serial.print(ssid);
+    WiFi.begin(ssid, password);      
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+      retrycount += 1;
+    }
+    if (WiFi.status() == WL_CONNECTED) {
+      Serial.println(WiFi.localIP());
+      String s_ip = WiFi.localIP().toString();
+      s_ip = s_ip.substring(s_ip.length() - 3, s_ip.length());
+      int i_ip = s_ip.toInt();
+      display.clear();
+      display.showNumberDec(i_ip);
+      delay(3000);
+      configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+      if (getLocalTime(&timeinfo)) {
+        Serial.println(&timeinfo, "%Y/%m/%d %H:%M:%S");
+        timesync = true;
+      } else {
+        delay(30000);
+      }
+    }
+  }
+}
+
+void setup()
+{
+//  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  Serial.begin(115200);
+  pinMode(led, OUTPUT); //working flashing
+  pinMode(buzzer, OUTPUT); //busser
+  digitalWrite(buzzer, true);
+  delay(500);
+  digitalWrite(buzzer, false);
+  
+  preferences.begin("Ehome", false);
+  int i_CT_ADJ = preferences.getUInt("CT_ADJ", CT_ADJ_factory);
+  CT_ADJ = i_CT_ADJ / 10.0;
+  Serial.print("CT_ADJ=");
+  Serial.println(CT_ADJ);
+  alert = preferences.getUInt("ALERT", alert_factory);
+  Serial.print("ALERT=");
+  Serial.println(alert);
+  cycle = preferences.getUInt("CYCLE", cycle_factory);
+  Serial.print("CYCLE=");
+  Serial.println(cycle);
+
+  display.setBrightness(4);
+  display.clear();
+
+  digitalWrite(led, true);
+  initwifi();  
+  digitalWrite(led, false);
+  digitalWrite(buzzer, true);
+  delay(500);
+  digitalWrite(buzzer, false);
+
+  analogSetAttenuation(ADC_11db);
+  emon1.current(CT_ADC, CT_ADJ);             // 36, Current: input pin, calibration. 60, 11.7
+
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 1000000, true);
+  timerAlarmEnable(timer);
+
+  if (!SD.begin(SD_CS_Pin)) {
+    Serial.println("SD failed");
+  }
+  digitalWrite(led, true);
+
+  cycle_cnt = -10; // let stablize
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    getLocalTime(&timeinfo);
+    char timeStringBuff[22];
+    strftime(timeStringBuff, sizeof(timeStringBuff), "%Y/%m/%d %H:%M:%S", &timeinfo);
+    Webmessage = timeStringBuff;
+    Webmessage += " - ";
+    Webmessage += Irms / 100.00;
+    Webmessage += "A";
+    request->send(200, "text/plain", Webmessage);
+  });
+  
+  server.on("/history", HTTP_GET, [](AsyncWebServerRequest *request){
+    Webmessage = "";
+    historyAction = SD.open("/history.csv", FILE_READ);
+    while (historyAction.available()) {
+      Webmessage += historyAction.readStringUntil('\n');
+      //Serial.println(Webmessage); //Printing for debugging purpose         
+    }
+    historyAction.close();
+    request->send(200, "text/plain", Webmessage);
+  });
+
+  server.on("/clear", HTTP_GET, [](AsyncWebServerRequest *request){
+    historyAction = SD.open("/history.csv", FILE_WRITE);
+    if (historyAction) {
+      historyAction.println("Date_Time,Amp");
+      historyAction.close();
+      Webmessage = "History Cleared";
+    } else {
+      Serial.println("SD error");
+      Webmessage = "SD clear error";        
+    }
+
+    request->send(200, "text/plain", Webmessage);
+  });
+
+  server.on("/ctadj", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(request->hasParam("value")) {
+      String s_ctadj = request->getParam("value")->value();
+      Serial.println(s_ctadj);
+      double d_ctadj = s_ctadj.toDouble();
+      if (d_ctadj > 0) {
+        CT_ADJ = d_ctadj;
+        preferences.putUInt("CT_ADJ", (CT_ADJ * 10));
+        emon1.current(CT_ADC, CT_ADJ);   
+        Webmessage = "CT ADJ set to " + String(CT_ADJ, 1);
+      } else {
+        Webmessage = "Input Error";
+      }
+    } else {
+      Webmessage = "No input";      
+    }
+    request->send(200, "text/plain", Webmessage);     
+  });
+
+  server.on("/alert", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(request->hasParam("value")) {
+      String s_alert = request->getParam("value")->value();
+      Serial.println(s_alert);
+      int d_alert = s_alert.toInt();
+      if (d_alert > 0) {
+        alert = d_alert;
+        preferences.putUInt("ALERT", alert);
+        Webmessage = "ALERT set to " + String(alert);
+      } else {
+        Webmessage = "Input Error";
+      }
+    } else {
+      Webmessage = "No input";      
+    }
+    request->send(200, "text/plain", Webmessage);     
+  });
+
+  server.on("/cycle", HTTP_GET, [](AsyncWebServerRequest *request){
+    if(request->hasParam("value")) {
+      String s_cycle = request->getParam("value")->value();
+      Serial.println(s_cycle);
+      int d_cycle = s_cycle.toInt();
+      if (d_cycle > 0) {
+        cycle = d_cycle;
+        preferences.putUInt("CYCLE", cycle);
+        Webmessage = "CYCLE set to " + String(cycle);
+      } else {
+        Webmessage = "Input Error";
+      }
+    } else {
+      Webmessage = "No input";      
+    }
+    request->send(200, "text/plain", Webmessage);     
+  });
+
+  server.begin();
+  digitalWrite(led, false);
+  digitalWrite(buzzer, true);
+  delay(500);
+  digitalWrite(buzzer, false);
+}
+
+void loop()
+{
+  Serial.print("Cycle=");
+  start = millis();
+  Irms = round(emon1.calcIrms(cycle) * 100.00);
+  Serial.print(millis() - start);
+  Serial.print(",Irms=");
+  Serial.print(Irms / 100.00);
+  Serial.print(", last_Irms=");
+  Serial.print(last_Irms / 100.00);
+  Serial.print(", Diff=");
+  Serial.print(abs(Irms - last_Irms));
+  Serial.print(", cycle_cnt=");
+  Serial.print(cycle_cnt);
+  changed = false;
+//  if ( (abs(Irms - last_Irms) * 1.00 / last_Irms * 1.00) > changed_pct) { //diff > x%
+  if (abs(Irms - last_Irms) > changed_A) { //diff > x A * 100
+    if (cycle_cnt > changed_duration) { //diff > x% for y seconds
+      changed = true;
+      last_Irms = Irms;
+    }
+  } else {
+    cycle_cnt = 0;
+  }  
+  Serial.print(", changed=");
+  Serial.print(changed);
+  if (changed) {
+    display.clear();
+    showIrms(last_Irms);
+    getLocalTime(&timeinfo);
+    history = SD.open("/history.csv", FILE_APPEND);
+    if (history) {
+      history.print(&timeinfo, "%Y/%m/%d %H:%M:%S");
+      history.print(",");
+      history.println(last_Irms / 100.00);
+      history.close();
+      Serial.print(&timeinfo, "%Y/%m/%d %H:%M:%S");
+      Serial.print(",");
+      Serial.println(last_Irms / 100.00);
+    } else {
+      Serial.println("SD error");        
+    }
+  }
+  Serial.print(", wifi=");
+  Serial.println(WiFi.status());
+
+  digitalWrite(led, false);
+  if (WiFi.status() != WL_CONNECTED) {
+    reconnect_cnt += 1;
+    if (reconnect_cnt > 30) {
+      digitalWrite(led, true);
+      //WiFi.disconnect();
+      WiFi.reconnect();
+      Serial.println("Reconnecting...");  
+      reconnect_cnt = 0;
+    }
+  }
+      
+  if (Irms != last_Irms) {
+    showIrms(Irms);   
+  }
+  
+  if (last_Irms >= alert) {
+    if (state) {
+      digitalWrite(buzzer, true);      
+    } else {
+      digitalWrite(buzzer, false);
+    }
+  } else {
+    digitalWrite(buzzer, false);                
+  }
+
+  delay(1000);
+}
+
+void showIrms(double value) {
+  len = 3;
+  pos = 1;
+  dot = 0x80;
+  if (value > 999) {
+    len = 4;
+    pos = 0;
+    dot = 0x40;
+  } else {
+    display.setSegments(OFF, 1, 0);
+  }
+  display.showNumberDecEx(value, dot, true, len, pos);
+}
---
+++ doc/README.md
@@ -0,0 +1,132 @@
+# eHome
+*Monitor Home electricity consumption using CT & ESP32*
+### Author: Michael Leung from Hong Kong
+
+## Intro
+![eHome](/doc/IMG_5466.jpg)
+
+This device provides the following functions:
+* Monitor home electricity consumption per second with realtime display in Ampere
+* With 10% change for 10 seconds, record to SD card
+* Trigger alarm when over predetermine Ampere
+* HTTP get current consumption
+* HTTP get history
+* HTTP restful API to adjust parameters
+
+## References
+* https://learn.openenergymonitor.org/electricity-monitoring/ct-sensors/interface-with-arduino
+* https://github.com/openenergymonitor/EmonLib
+* https://github.com/avishorp/TM1637.git
+* https://github.com/me-no-dev/ESPAsyncWebServer.git
+* https://community.openenergymonitor.org/t/sampling-rate-of-emonlib/4383
+
+## Components
+* Current Transformer: https://www.ebay.com/itm/174480353810
+* ESP32: https://www.amazon.com/DORHEA-Bluetooth-Internet-Development-Functional/dp/B08ND91YB8?th=1
+* SD card module: https://www.ebay.com/itm/293683210627
+* LED display: https://www.amazon.com/diymore-Display-7-Segment-4-Digit-Digital/dp/B078J2BRRB
+* 3.5mm socket x 2
+* buzzer
+* 10k resistor x 2
+* 10uf capacitor
+* 0.1uf capacitor
+* Diode IN4004
+* plastic box: https://www.ebay.com/itm/383751696130 
+* double sided circult board: https://www.amazon.com/GFORTUN-Protoboard-Prototyping-Soldering-Universal/dp/B06XR1JWNY
+* USB charging cable
+* 3.5mm plug
+
+## Schematic circuit diagram
+![Schematic](/doc/ehome.png)
+
+## Circuit Board design
+![Schematic](/doc/ehome_circuit.png)
+
+## Assembly
+![Assembly](/doc/IMG_5459.jpg)
+Solder the bottom side, with ESP32 socket, buzzer, 5v 3.5mm plug, diode and wiring.
+As seen in the picture, you need to cut one side of the circuit board to fit into the box. The 4 pin socket on the side you have cut is just a dummy to make the circuit board sitting flat.<br />
+Prepare LED display on the cover top. Don't mount and drill holes for the 4 pins connector yet. Needs precise alignment with the top side socket.
+
+![Assembly](/doc/IMG_5460.jpg)
+Solder the top side, with resistors, capacitors, 3.5mm socket for SCT-13-060, 4 pins sockets for LED display and 6 pins socket for SD card module. I used the 0.1uf capacitor to mount the 3.5mm socket on the board because I want to cancel noise from the outset.<br />
+Mount the SD card module on the cover bottom. Need careful alignment with the 6 pins socket. Mount the LED display on cover top with holes for the 4 pins. Again, need careful aligment with the 4 pins socket.
+
+![Assembly](/doc/IMG_5462.jpg)
+Plug in ESP32, and the cover assembly with both LED display and SD module onto the respective sockets.
+
+![Assembly](/doc/IMG_5463.jpg)
+Drill holes on each side of the plastic box for the two 3.5mm sockets. Again, you need very careful alignment. <br />
+Close the plastic box and fasten the 3.5mm socket nuts.
+
+![Assembly](/doc/IMG_5471.jpg)
+**Warning!** Employ a licensed eletrical technician to plug in the SCT-13-060 to your home's main supply.
+
+![eHome](/doc/IMG_5466.jpg)
+Cut one end of the USB cable and replace it with the 3.5mm plug. I used a wall plug with USB charging to supply the +5v power supply.
+
+## HTTP functions
+You have to hard code 
+* your wifi routers SSID and Password
+* your region's NTP server URL
+
+### 1. Current consumption http://xxx.xxx.x.xxx/
+```
+2021/10/31 00:05:52 - 1.55A
+```
+
+### 2. History http://xxx.xxx.x.xxx/history
+```
+Date_Time,Amp
+2021/10/28 14:17:14,2.76
+2021/10/28 14:21:34,0.63
+2021/10/28 19:51:35,7.74
+```
+### 3. Clear all History records http://xxx.xxx.x.xxx/clear
+
+### 4. Set alert level http://xxx.xxx.x.xxx/alert?value=3300
+3300 with assumed 2 decimal point = 33.00A
+
+### 5. CT Calibration http://xxx.xxx.x.xxx/ctadj?value=60
+We need a "Current Constant" to create the emonlib object.
+```
+emon1.current(ESP32 ADC pin, Current Constant);
+```
+Based on the calibration theory on learn.openenergymonitor.org, If you use a current transformer with a built-in burden (SCT-013-060 is one of these), then
+```
+current constant = max primary current ÷ (max secondary current (I) * burden reisiter (R))
+current constant = max primary current ÷ (max voltage across the burden reisiter)
+```
+For SCT-013-060, we know "max primary current" = 60 and "max voltage across the burden reisiter" = 1V. Hence, 
+```
+current constant = 60 ÷ (1) = 60
+emon1.current(ESP32 ADC pin, 60);
+```
+### 6. Sampling cycle Calibration http://xxx.xxx.x.xxx/cycle?value=5394
+Our electricity supply is in form of Alternating Current (AC), which is the number of cycles per second of sine waves in Hertz (Hz). Best sampling rates are complete multiple of complete sin wave. The sampling-rate-of-emonlib reference above found in his setup that:
+```
+calcIrms( ) will have approx 5588 current samples per second
+1 cycle of mains:
+112 for a 50 Hz system, or 93 for a 60 Hz system.
+
+The smallest ‘universal’ number:
+559 (100 ms, or 5 cycles of 50 Hz or 6 cycles of 60 Hz).
+
+The recommended monitoring period:
+1676 (300 ms).
+```
+However, in my setup, cycles to last for 300 ms is 5394.
+```
+  Serial.print("Cycle=");
+  start = millis();
+  Irms = emon1.calcIrms(5394);
+  Serial.println(millis() - start);
+
+output:
+Cycle=300
+```
+
+
+
+
+
